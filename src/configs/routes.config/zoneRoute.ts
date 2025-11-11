9288db5e6e3b3cb4b51fcc5f91f7d71dd67af313
ek-rahees <rahees.ek.raihsoft@gmail.com>
2025-11-11 16:23:22 +0530

    update orderroute

--- src/configs/routes.config/zoneRoute.ts
+++ src/configs/routes.config/zoneRoute.ts
@@ -11,20 +11,20 @@ const zoneRoute: Routes = [
     //     component: lazy(() => import("@/views/order/zone/CustomerCreate")),
     //     authority: [ADMIN, USER],
     // },
-        {
-            key: "zoneEdit",
-            path: `${ZONE_EDIT_PREFIX_PATH}/:id`,           // /employee-edit/:id
-            component: lazy(() => import("@/views/order/zone/CustomerEdit")),
-            authority: [ADMIN, USER],
-            meta: {
-                header: {
-                    title: "Edit Zone",
-                    description: "Manage zone details, profile, and preferences.",
-                    contained: true,
-                },
-                footer: false,
-            },
-        },
+        // {
+        //     key: "zoneEdit",
+        //     path: `${ZONE_EDIT_PREFIX_PATH}/:id`,           // /employee-edit/:id
+        //     component: lazy(() => import("@/views/order/zone/CustomerEdit")),
+        //     authority: [ADMIN, USER],
+        //     meta: {
+        //         header: {
+        //             title: "Edit Zone",
+        //             description: "Manage zone details, profile, and preferences.",
+        //             contained: true,
+        //         },
+        //         footer: false,
+        //     },
+        // },
             {
                 key: "employeeDeatails",
                 path: `${ZONE_DEATILS_PREFIX_PATH}/:id`,
